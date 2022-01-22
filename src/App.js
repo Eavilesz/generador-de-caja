c9ae26544ab809e7bc564d360bccfa55b6fd8d35
Eavilesz <ernesto-av@hotmail.com>
2022-01-21 22:19:22 -0500

    initial comit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import AddBox from "./components/AddBox";
+
+function App() {
+  return (
+    <div className="App">
+      <AddBox />
+    </div>
+  );
+}
+
+export default App;
